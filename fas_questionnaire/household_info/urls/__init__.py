f15e45ebdbf443faef64be04fad8d71cb125f91f
aamer mohammed <aamermail@gmail.com>
2017-03-29 06:14:20 +0530

    removed baker dependency

--- fas_questionnaire/household_info/urls/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^info/', include('household_info.urls.household_urls')),
-    url(r'^members/', include('household_info.urls.household_members_urls')),
-]
--- fas_questionnaire/household_info/views/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from .household_views import *
-from .household_members_views import *

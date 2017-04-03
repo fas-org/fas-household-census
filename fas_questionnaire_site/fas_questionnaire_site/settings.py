119f473fd3155555da5d69abe1f97f01796fc4e1
aamer mohammed <aamermail@gmail.com>
2017-04-03 19:05:49 +0530

    added mysql settings

--- fas_questionnaire_site/fas_questionnaire_site/settings.py
+++ fas_questionnaire_site/fas_questionnaire_site/settings.py
@@ -79,13 +79,19 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'fasdb',
+    #     'USER': 'fas',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fasdb',
-        'USER': 'fas',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fas',
+        'USER': 'root',
+        'PASSWORD': ''
+     }
 }
 
 

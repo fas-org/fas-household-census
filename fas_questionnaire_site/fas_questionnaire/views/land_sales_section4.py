88164b6651a4911cc2ba928ca2908771172e9a22
Anil Kumar <aniltpl08@gmail.com>
2017-04-06 19:41:07 +0530

    [Anil] added login require for section 4

--- fas_questionnaire_site/fas_questionnaire/views/land_sales_section4.py
+++ fas_questionnaire_site/fas_questionnaire/views/land_sales_section4.py
@@ -2,8 +2,10 @@ from ..forms.land_sales_forms_section4 import LandSoldForm, LandPurchasedForm
 from ..models.land_sales_models_section4 import LandSold, LandPurchased
 from django.shortcuts import get_object_or_404, render, redirect
 from . import household as household
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def init(request):
     if request.session.get('household') is None:
         return new(request)
@@ -11,6 +13,7 @@ def init(request):
         return edit(request, request.session['household'])
 
 
+@login_required(login_url='login')
 def new(request):
     if request.method == "POST":
         landsoldform = LandSoldForm(request.POST)
@@ -22,39 +25,50 @@ def new(request):
             landsold.save()
 
             landpurchased = landpurchasedform.save(commit=False)
-            landpurchased.household = household.get(request.session['household'])
+            landpurchased.household = household.get(
+                request.session['household'])
             landpurchased.save()
 
-            return redirect('landsales_edit', household=request.session['household'])
+            return redirect('landsales_edit',
+                            household=request.session['household'])
     else:
         landsoldform = LandSoldForm()
         landpurchasedform = LandPurchasedForm()
-    return render(request, 'land_sales_section4.html', {'landsold_form': landsoldform, 'landpurchased_form': landpurchasedform })
+    return render(request, 'land_sales_section4.html',
+                  {'landsold_form': landsoldform,
+                   'landpurchased_form': landpurchasedform})
 
 
-def edit(request, household):
-    landsold = get_object_or_404(LandSold, household=household)
-    landpurchased = get_object_or_404(LandPurchased, household=household)
+@login_required(login_url='login')
+def edit(request, pk):
+    try:
+        landsold = get_object_or_404(LandSold, household=pk)
+        landpurchased = get_object_or_404(LandPurchased, household=pk)
 
-    if request.method == "POST":
-        landsoldform = LandSoldForm(request.POST, instance=landsold)
-        landpurchasedform = LandPurchasedForm(request.POST, instance=landpurchased)
+        if request.method == "POST":
+            landsoldform = LandSoldForm(request.POST, instance=landsold)
+            landpurchasedform = LandPurchasedForm(request.POST,
+                                                  instance=landpurchased)
 
-        if landsoldform.is_valid() and landpurchasedform.is_valid():
+            if landsoldform.is_valid() and landpurchasedform.is_valid():
+                landsold = landsoldform.save(commit=False)
+                landsold.save()
 
-            landsold = landsoldform.save(commit=False)
-            landsold.save()
+                landsold = landpurchasedform.save(commit=False)
+                landsold.save()
 
-            landsold = landpurchasedform.save(commit=False)
-            landsold.save()
+                return redirect('landsales_edit', pk=pk)
+        else:
+            landsoldform = LandSoldForm(instance=landsold)
+            landpurchasedform = LandPurchasedForm(instance=landpurchased)
+        return render(request, 'land_sales_section4.html',
+                      {'landsold_form': landsoldform,
+                       'landpurchased_form': landpurchasedform})
+    except Exception:
+        return new(request)
 
-            return redirect('landsales_edit', pk=household)
-    else:
-        landsoldform = LandSoldForm(instance=landsold)
-        landpurchasedform = LandPurchasedForm(instance=landpurchased)
-    return render(request, 'land_sales_section4.html', {'landsold_form': landsoldform, 'landpurchased_form':landpurchasedform })
 
-
+@login_required(login_url='login')
 def get(household):
     try:
         landsold = LandSold.objects.get(household=household)

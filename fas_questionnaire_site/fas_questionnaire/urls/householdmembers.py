6293270e7a3fc1bc3973b88ec9dc4124a804e824
Srujan Kumar Bojjam <srujan@swecha.net>
2017-04-08 00:46:47 +0530

    add household members section edit flow

--- fas_questionnaire_site/fas_questionnaire/urls/householdmembers.py
+++ fas_questionnaire_site/fas_questionnaire/urls/householdmembers.py
@@ -4,7 +4,5 @@ from ..views import householdmembers
 urlpatterns = [
     url(r'^$', householdmembers.new, name='householdmembers_new'),
     url(r'^init$', householdmembers.init, name='householdmembers_init'),
-    url(r'^save$', householdmembers.save, name='householdmembers_save'),
     url(r'^(?P<pk>\d+|None)$', householdmembers.edit, name='householdmembers_edit'),
-    # url(r'^get$', householdmembers.get, name='householdmembers_get'),
 ]
--- fas_questionnaire_site/fas_questionnaire/views/householdmembers.py
+++ fas_questionnaire_site/fas_questionnaire/views/householdmembers.py
@@ -14,49 +14,44 @@ def init(request):
     if request.session.get('household') is None:
         return new(request)
     else:
+        result_set = HouseholdMembers.objects.filter(household=request.session.get('household'))
+        if len(result_set) == 0:
+            return new(request)
         return edit(request, request.session['household'])
 
 
 @login_required(login_url='login')
 def new(request):
-    class RequiredFormSet(BaseFormSet):
-        def __init__(self, *args, **kwargs):
-            super(RequiredFormSet, self).__init__(*args, **kwargs)
-            for form in self.forms:
-                form.empty_permitted = False
-
-    householdMembersFormSet = formset_factory(HouseholdMembersForm, formset=BaseFormSet, extra=2)
+    household_members_formset = formset_factory(HouseholdMembersForm, formset=BaseFormSet, extra=5)
     if request.method == "POST":
-        forms = householdMembersFormSet(request.POST)
+        forms = household_members_formset(request.POST)
 
         if forms.is_valid():
             for form in forms:
-                if form.is_valid():
+                if form.is_valid() and form.has_changed():
                     member = form.save(commit=False)
-                    member.household = Household.get(request.session['household'])
+                    member.household = Household.objects.get(pk=request.session['household'])
                     member.save()
             return redirect('householdmembers_edit', pk= request.session['household'])
 
-    return render(request, 'householdmembers.html', { 'formset': householdMembersFormSet})
+    return render(request, 'householdmembers.html', { 'formset': household_members_formset})
 
 
 @login_required(login_url='login')
 def edit(request, pk):
-    householdMembersFormSet = modelformset_factory(HouseholdMembers,form=HouseholdMembersForm)
-    result_set = HouseholdMembers.objects.filter(household=pk)
-    formset = householdMembersFormSet(queryset=result_set)
-    return render(request, 'householdmembers.html', { 'formset': formset})
-
-@login_required(login_url='login')
-def save(request):
     if request.method == "POST":
-        posted_forms = request.POST.getlist('forms[]')
-        for posted_form in posted_forms:
-            form_data = json.loads(posted_form)
-            form = HouseholdMembersForm(form_data)
-            if form.is_valid():
-                household_members = form.save(commit=False)
-                household_members.household = Household.get(request.session['household'])
-                household_members.save()
-        messages.success(request, "Records saved successfully")
-        return redirect('householdmembers_edit', pk= request.session['household'])
+        household_members_formset = formset_factory(HouseholdMembersForm, formset=BaseFormSet, extra=5)
+        forms = household_members_formset(request.POST)
+        HouseholdMembers.objects.filter(household=pk).delete()
+
+        if forms.is_valid():
+            for form in forms:
+                if form.is_valid() and form.has_changed():
+                    member = form.save(commit=False)
+                    member.household = Household.objects.get(pk=request.session['household'])
+                    member.save()
+
+    household_members_model_formset = modelformset_factory(HouseholdMembers,form=HouseholdMembersForm, extra=5)
+    result_set = HouseholdMembers.objects.filter(household=pk)
+    formset = household_members_model_formset(queryset=result_set)
+    return render(request, 'householdmembers.html', { 'formset': formset})

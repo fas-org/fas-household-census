867493e3b2cf1d8be8b0ff11231d567b0483c853
Masiur Rahaman <rmasiur@thoughtworks.com>
2017-04-17 14:55:50 +0530

    adding table 5.3.2 Land leased out on share-rent

--- fas_questionnaire_site/fas_questionnaire/forms/page4.py
+++ fas_questionnaire_site/fas_questionnaire/forms/page4.py
@@ -2,6 +2,7 @@ from django import forms
 from ..models.page4 import LandMortgagedIn
 from ..models.page4 import LandMortgagedOut
 from ..models.page4 import LandLeasedInOnShareRent
+from ..models.page4 import LandLeasedOutOnShareRent
 
 
 class LandMortgagedInForm(forms.ModelForm):
@@ -86,6 +87,53 @@ class LandLeasedInOnShareRentForm(forms.ModelForm):
                   'amount_of_other_loan',
                   'interest_of_other_loan',
                   'comment',]
+        exclude = ['household_number']
+        widgets = None
+        localized_fields = None
+        labels = {}
+        help_texts = {}
+        error_messages = {}
+
+
+class LandLeasedOutOnShareRentForm(forms.ModelForm):
+    class Meta:
+        model = LandLeasedOutOnShareRent
+        fields = ['household_number',
+                  'ownership_plot_no',
+                  'land_type',
+                  'extent',
+                  'name_of_sharecropper',
+                  'caste_of_sharecropper',
+                  'occupation_of_sharecropper',
+                  'registered_unregistered',
+                  'seasonal_annual_other',
+                  'year_of_lease',
+                  'percentage_share_of_crop',
+                  'quantity_share_of_crop',
+                  'percentage_share_of_by_product',
+                  'quantity_share_of_by_product',
+                  'share_in_fym_field',
+                  'share_in_fym_rs_field',
+                  'share_in_fertiliser_field',
+                  'share_in_fertiliser_rs_field',
+                  'share_in_seed_field',
+                  'share_in_seed_rs_field',
+                  'share_in_pesticide_field',
+                  'share_in_pesticide_rs_field',
+                  'share_in_electricity_field',
+                  'share_in_electricity_rs_field',
+                  'share_in_pumpset_field',
+                  'share_in_pumpset_rs_field',
+                  'share_in_labour_field',
+                  'share_in_labour_quantity_field',
+                  'share_in_irrigation_field',
+                  'share_in_irrigation_rs_field',
+                  'share_in_machinery_field',
+                  'share_in_machinery_quantity_field',
+                  'interest_free_loan_by_owner',
+                  'amount_of_other_loan',
+                  'interest_of_other_loan',
+                  'comment']
         exclude = ['household_number']
         widgets = None
         localized_fields = None

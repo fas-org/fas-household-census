8f654d25254286ebf0bf8ff060c3ffd6db6e2de9
Anil Kumar <aniltpl08@gmail.com>
2017-04-19 16:27:58 +0530

    [Anil] added resource of finance as dropdown in section 9b

--- fas_questionnaire_site/fas_questionnaire/models/common.py
+++ fas_questionnaire_site/fas_questionnaire/models/common.py
@@ -69,3 +69,15 @@ class Caste(models.Model):
 
     def __str__(self):
         return self.caste
+
+
+class Units(models.Model):
+    id = models.AutoField(primary_key=True)
+    unit = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'units'
+
+    def __str__(self):
+        return self.unit

bfd57071418b3c92aa7e8020175790701179af07
salauddin <snalband@thoughtworks.com>
2017-04-20 19:15:58 +0530

    Salauddin|page 17| Added 19A section

--- fas_questionnaire_site/fas_questionnaire/forms/page17.py
+++ fas_questionnaire_site/fas_questionnaire/forms/page17.py
@@ -11,3 +11,15 @@ class IncomeFromSalariesForm(forms.ModelForm):
         labels = {}
         help_texts = {}
         error_messages = {}
+class IncomeFromOtherBusinessActivitiesForm(forms.ModelForm):
+    class Meta:
+        model=IncomeFromOtherBusinessActivities
+        exclude=['household']
+        widgets=None
+        localized_fields=None
+        labels={
+            'rents_paid':'Rents Paid(for land,building or machine)',
+            'rents_paid':'Any other costs(specify)'
+        }
+        help_texts={}
+        error_messages={}
--- fas_questionnaire_site/fas_questionnaire/models/page17.py
+++ fas_questionnaire_site/fas_questionnaire/models/page17.py
@@ -16,4 +16,24 @@ class IncomeFromSalaries(models.Model):
 
     class Meta:
         managed = True
-        db_table = 'Salaries artisan earnings other businesses'
+        db_table = 'Income from salaries artisan earnings'
+
+
+class IncomeFromOtherBusinessActivities(models.Model):
+    id = models.AutoField(primary_key=True)
+    household = models.ForeignKey(Household, models.DO_NOTHING, db_column='household')
+    name_of_worker = models.ForeignKey(HouseholdMembers, models.DO_NOTHING, db_column='Name of worker')
+    description_of_work = models.CharField(db_column='Description of work', max_length=50, blank=True, null=True)
+    place_of_work = models.CharField(db_column='Place of work', max_length=50, blank=True, null=True)
+    production_and_sale = models.CharField(db_column='Production and sale', max_length=50, blank=True, null=True)
+    cost_of_material_inputs = models.CharField(db_column='Cost of material inputs', max_length=50, blank=True,
+                                               null=True)
+    wages_and_salaries_paid = models.CharField(db_column='Wages and salaries paid', max_length=50, blank=True,
+                                               null=True)
+    rents_paid = models.CharField(db_column='Rents paid', max_length=50, blank=True, null=True)
+    any_other_costs = models.CharField(db_column='Any other costs', max_length=50, blank=True, null=True)
+    comments = models.CharField(db_column='Comments', max_length=250, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'Income from other business activities'

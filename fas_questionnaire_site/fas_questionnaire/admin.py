fd0e31da073b6f3caff96786f464473609eb4531
rgajula <rgajula@inrgajula.local>
2017-04-28 11:10:44 +0530

    Rasheeda/Ravindra changes as per testing done on  page 22,20 and 1

--- fas_questionnaire_site/fas_questionnaire/admin.py
+++ fas_questionnaire_site/fas_questionnaire/admin.py
@@ -1,28 +1,38 @@
 from django.contrib import admin
-from .models.household_models import Household, Village
+from .models.common import Sex, Crop, PlaceOfWork, Caste, LandType
+from .models.household_models import Village
+from .models.page1 import LiteracyStatus, MaritalStatus, CalendarGranularity, Relationship, Occupation, Education, Religion, TehsilOfBirth
 from .models.page10 import OtherCostsItems
 from .models.page_13 import WageUnit, TypeOfWage
-from .models.page1 import LiteracyStatus, MaritalStatus, CalendarGranularity
-from .models.common import Sex, Crop, PlaceOfWork, Caste, LandType
 from .models.page20 import SourceOfBorrowing, PurposeOfBorrowing, MonthWhenFullyRepaid, BankNgoToWhichTheGroupIsLinked, PeriodOfMembership, NameOfBankPostOffice, TypeOfAccount
 from .models.page22 import InvestigationNeeded
 
-
-admin.site.register(OtherCostsItems)
-admin.site.register(LiteracyStatus)
-admin.site.register(MaritalStatus)
-admin.site.register(CalendarGranularity)
-admin.site.register(Village)
+#Common
 admin.site.register(Sex)
 admin.site.register(Crop)
 admin.site.register(PlaceOfWork)
-admin.site.register(WageUnit)
-admin.site.register(TypeOfWage)
 admin.site.register(Caste)
 admin.site.register(LandType)
 
-#Page 22
-admin.site.register(InvestigationNeeded)
+#household_models
+admin.site.register(Village)
+
+#Page 1
+admin.site.register(CalendarGranularity)
+admin.site.register(Relationship)
+admin.site.register(LiteracyStatus)
+admin.site.register(MaritalStatus)
+admin.site.register(Occupation)
+admin.site.register(Education)
+admin.site.register(Religion)
+admin.site.register(TehsilOfBirth)
+
+#Page 10
+admin.site.register(OtherCostsItems)
+
+#Page 13
+admin.site.register(WageUnit)
+admin.site.register(TypeOfWage)
 
 #Page 20
 admin.site.register(SourceOfBorrowing)
@@ -32,3 +42,6 @@ admin.site.register(BankNgoToWhichTheGroupIsLinked)
 admin.site.register(PeriodOfMembership)
 admin.site.register(NameOfBankPostOffice)
 admin.site.register(TypeOfAccount)
+
+#Page 22
+admin.site.register(InvestigationNeeded)

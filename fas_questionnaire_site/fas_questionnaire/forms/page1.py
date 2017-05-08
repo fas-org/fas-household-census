8b5b7dbef231414ce1da8acf995416619bb6029d
shiri134 <kshiri@thoughtworks.com>
2017-05-08 16:44:14 +0530

    salauddin,shirisha | page1 | Added option to enter other-caste in caste drop down

--- fas_questionnaire_site/fas_questionnaire/forms/page1.py
+++ fas_questionnaire_site/fas_questionnaire/forms/page1.py
@@ -1,4 +1,7 @@
 from django import forms
+
+from fas_questionnaire.forms.common import ListTextWidget
+from fas_questionnaire.models.common import Caste
 from ..models.page1 import HouseholdIntroduction, HouseholdMembers
 
 
@@ -8,11 +11,15 @@ class HouseholdIntroductionForm(forms.ModelForm):
     class Meta:
         model = HouseholdIntroduction
         exclude = ['household']
-        widgets = None
         localized_fields = None
         labels = {}
         help_texts = {}
         error_messages = {}
+
+    def __init__(self, *args, **kwargs):
+        super(HouseholdIntroductionForm, self).__init__(*args, **kwargs)
+        caste_list = Caste.objects.values_list('caste')
+        self.fields['caste_tribe'].widget = ListTextWidget(data_list=caste_list, name='caste-list')
 
 
 class HouseholdMembersForm(forms.ModelForm):

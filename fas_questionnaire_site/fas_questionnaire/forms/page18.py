371636d7b422f53377424fd6191651595c77a3e3
Rasheeda K <krashee@thoughtworks.com>
2017-05-15 17:16:17 +0530

    Rasheeda|Added text to dropdowns in page18

--- fas_questionnaire_site/fas_questionnaire/forms/page18.py
+++ fas_questionnaire_site/fas_questionnaire/forms/page18.py
@@ -1,4 +1,7 @@
 from django import forms
+
+from fas_questionnaire.forms.common import ListTextWidget
+from fas_questionnaire.models.common import YesOrNo
 from ..models.page18 import *
 
 
@@ -11,6 +14,17 @@ class AcquisitionAndLossOfMajorAssetsForm(forms.ModelForm):
         help_texts = {}
         error_messages = {}
 
+    def __init__(self, *args, **kwargs):
+        super(AcquisitionAndLossOfMajorAssetsForm, self).__init__(*args, **kwargs)
+        assets_list = DescriptionOfAssets.objects.values_list('asset_name')
+        self.fields['description_of_asset'].widget = ListTextWidget(data_list=assets_list, name='assets-list')
+
+        month_list = Month.objects.values_list('month')
+        self.fields['month_of_sale'].widget = ListTextWidget(data_list=month_list, name='month_list')
+
+        month_list = Month.objects.values_list('month')
+        self.fields['month_of_purchase'].widget = ListTextWidget(data_list=month_list, name='month_list')
+
 class ForChildrenOfAge616YearsForm(forms.ModelForm):
     class Meta:
         model = ForChildrenOfAge616Years
@@ -19,3 +33,12 @@ class ForChildrenOfAge616YearsForm(forms.ModelForm):
         localized_fields = None
         help_texts = {}
         error_messages = {}
+
+    def __init__(self, *args, **kwargs):
+        super(ForChildrenOfAge616YearsForm, self).__init__(*args, **kwargs)
+
+        whether_enrolled_in_eductional_institution_currently_list = YesOrNo.objects.values_list('title')
+        self.fields['whether_enrolled_in_eductional_institution_currently'].widget = ListTextWidget(data_list=whether_enrolled_in_eductional_institution_currently_list, name='whether_enrolled_in_eductional_institution_currently_list')
+
+        whether_ever_enrolled_in_school_list = YesOrNo.objects.values_list('title')
+        self.fields['whether_ever_enrolled_in_school'].widget = ListTextWidget(data_list=whether_ever_enrolled_in_school_list,name='whether_ever_enrolled_in_school_list')
--- fas_questionnaire_site/fas_questionnaire/models/page18.py
+++ fas_questionnaire_site/fas_questionnaire/models/page18.py
@@ -16,17 +16,14 @@ class DescriptionOfAssets(models.Model):
         return self.asset_name
 
 
-
-
-
 class AcquisitionAndLossOfMajorAssets(models.Model):
     household = models.ForeignKey(Household, db_column='household')
     id = models.AutoField(primary_key=True)
     serial_no = models.IntegerField(db_column='Serial no',blank=True,null=True)
-    description_of_asset = models.ForeignKey(DescriptionOfAssets,db_column='Description of asset',blank=True,null=True)
-    month_of_sale = models.ForeignKey(Month, db_column='Month of sale',blank=True,null=True,related_name="%(class)s_Month_of_sale")
+    description_of_asset = models.CharField(max_length=100,db_column='Description of asset',blank=True,null=True)
+    month_of_sale = models.CharField(max_length=100,db_column='Month of sale',blank=True,null=True)
     price_received = models.FloatField(db_column='Price received', blank=True, null=True)
-    month_of_purchase = models.ForeignKey(Month, db_column='Month of purchase',blank=True,null=True,related_name="%(class)s_Month_of_purchase")
+    month_of_purchase = models.CharField(max_length=100,db_column='Month of purchase',blank=True,null=True)
     price_paid = models.FloatField(db_column='Price paid', blank=True, null=True)
     comments = models.CharField(max_length=255, blank=True, null=True)
 
@@ -38,11 +35,11 @@ class AcquisitionAndLossOfMajorAssets(models.Model):
 class ForChildrenOfAge616Years(models.Model):
     household = models.ForeignKey(Household, db_column='household')
     name = models.IntegerField(db_column='Name', blank=True, null=True)
-    whether_enrolled_in_eductional_institution_currently = models.NullBooleanField(db_column='Whether enrolled in eductional institution currently')  
+    whether_enrolled_in_eductional_institution_currently = models.CharField(max_length=100,db_column='Whether enrolled in eductional institution currently',blank=True,null=True)
     no_of_days_missed_school = models.IntegerField(db_column='No of days missed school', blank=True, null=True)  
     period_missed_school = models.CharField(db_column='period missed school', max_length=50, blank=True, null=True)  
     reason_for_missing_school = models.CharField(db_column='Reason for missing school', max_length=200, blank=True, null=True)  
-    whether_ever_enrolled_in_school = models.NullBooleanField(db_column='Whether ever enrolled in school')  
+    whether_ever_enrolled_in_school = models.CharField(max_length=100,db_column='Whether ever enrolled in school',blank=True,null=True)
     age_at_which_withdrawn = models.CharField(db_column='Age at which withdrawn', max_length=50, blank=True, null=True)  
     reasons_for_droppingout = models.CharField(db_column='reasons for droppingout', max_length=300, blank=True, null=True)
     comments = models.CharField(max_length=255, blank=True, null=True)

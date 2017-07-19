1065cfd1cff2ba6d5d55cbc0b13b61e708ea7de6
NavyaSriB <basava@thoughtworks.com>
2017-07-19 17:35:18 +0530

    Remove 'ownership plot no' column in page2 table3

--- fas_questionnaire_site/fas_questionnaire/models/page2.py
+++ fas_questionnaire_site/fas_questionnaire/models/page2.py
@@ -64,7 +64,6 @@ class CurrentOwnershipHolding(models.Model):
     id = models.AutoField(primary_key=True)
     sno = models.PositiveIntegerField(blank=True, null=True)
     household = models.ForeignKey(Household, models.DO_NOTHING, db_column='household')
-    ownership_plot_no = models.FloatField(db_column='Ownership plot no', blank=True, null=True)
     land_type = models.CharField(max_length=100,db_column='Land type', blank=True, null=True)
     extent_owned_land = models.CharField(max_length=100,db_column='Extent of owned land', blank=True, null=True)
     acquisition_mode = models.CharField(max_length=100,db_column='acquisition_mode', blank=True,
